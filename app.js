6cbc943548f5c3e63fa1acf8d4920edb01d41463
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-10-01 06:38:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,26 @@ app.use(bodyParser.json());
 var server = app.listen(3000,()=>{
     console.log('Server is running on port number 3000')
 })
-
+const basketball = io.of("/1");
+const basketball = io.of("/2");
+const basketball = io.of("/3");
+const basketball = io.of("/4");
+const basketball = io.of("/5");
+const basketball = io.of("/6");
+const basketball = io.of("/7");
+const basketball = io.of("/8");
+const basketball = io.of("/9");
+const basketball = io.of("/10");
+const basketball = io.of("/11");
+const basketball = io.of("/12");
+const basketball = io.of("/13");
+const basketball = io.of("/14");
+const basketball = io.of("/15");
+const basketball = io.of("/16");
+const basketball = io.of("/18");
+const basketball = io.of("/19");
+const basketball = io.of("/20");
+const basketball = io.of("/21");
 
 //Chat Server
 
