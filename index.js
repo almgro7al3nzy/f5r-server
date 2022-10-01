de4bc535389f86d6f6a74819b272b12cf8e76087
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-10-01 06:25:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -147,29 +147,8 @@ io.on('connection', (socket) => {
   });
 });
 
-const general = io.of("/general");
-const football = io.of("/football");
-const basketball = io.of("/1");
-const basketball = io.of("/2");
-const basketball = io.of("/3");
-const basketball = io.of("/4");
-const basketball = io.of("/5");
-const basketball = io.of("/6");
-const basketball = io.of("/7");
-const basketball = io.of("/8");
-const basketball = io.of("/9");
-const basketball = io.of("/10");
-const basketball = io.of("/11");
-const basketball = io.of("/12");
-const basketball = io.of("/13");
-const basketball = io.of("/14");
-const basketball = io.of("/15");
-const basketball = io.of("/16");
-const basketball = io.of("/17");
-const room-id = io.of("/18");
-const basketball = io.of("/19");
-const basketball = io.of("/20");
-const basketball = io.of("/21");
+
+
 const general = io.of("/general");
 const football = io.of("/football");
 const basketball = io.of("/basketball");
@@ -256,3 +235,24 @@ basketball.on('connection', function (socket) {
         socket.broadcast.emit('new_message', {nickname: msg.nickname, message: msg.message});
     });
 });
+const basketball = io.of("/1");
+const basketball = io.of("/2");
+const basketball = io.of("/3");
+const basketball = io.of("/4");
+const basketball = io.of("/5");
+const basketball = io.of("/6");
+const basketball = io.of("/7");
+const basketball = io.of("/8");
+const basketball = io.of("/9");
+const basketball = io.of("/10");
+const basketball = io.of("/11");
+const basketball = io.of("/12");
+const basketball = io.of("/13");
+const basketball = io.of("/14");
+const basketball = io.of("/15");
+const basketball = io.of("/16");
+const basketball = io.of("/17");
+const room-id = io.of("/18");
+const basketball = io.of("/19");
+const basketball = io.of("/20");
+const basketball = io.of("/21");
