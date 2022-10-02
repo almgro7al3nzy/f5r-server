97c6c5368a1b0c7abaf9761f73110c69eef6bbf0
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-10-02 14:25:15 +0300

    ffffffffff

--- config/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    port: process.env.PORT,
-    mongoUrl: process.env.DB_URL,
-    secret: process.env.SECRET,
-    stripe: process.env.STRIPE_SECRET
-}
